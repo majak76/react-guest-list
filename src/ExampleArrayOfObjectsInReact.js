50720e1f5b65cf08529ef8f5b5940686fe0eafc5
Maja Krmar <majakrmar05@gmail.com>
2023-05-16 21:46:29 +0200

    Add new functionalities

--- src/ExampleArrayOfObjectsInReact.js
+++ src/ExampleArrayOfObjectsInReact.js
@@ -1,7 +1,7 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
-export default function ExampleArrayOfObjectsInReact() {
-  const [user, setUser] = useState({
+const people = [
+  {
     gender: 'female',
     name: {
       title: 'Miss',
@@ -23,6 +23,32 @@ export default function ExampleArrayOfObjectsInReact() {
     uuid: '532dd481-383b-4efa-a5f3-6bea201937f5',
     phone: '(00) 6249-3485',
     picture: 'https://randomuser.me/api/portraits/med/women/12.jpg',
-  });
-  return <img alt="user pic" src={user.picture} />;
+  },
+];
+
+export default function ExampleArrayOfObjectsInReact() {
+  const [users, setUsers] = useState(people);
+
+  return (
+    <>
+      {users.map((user) => {
+        return (
+          <Fragment key={'user-profile-${user.uuid}'}>
+            <img alt="user pic" src={user.picture} />
+            <h6>
+              {user.name.title} {user.name.first} {user.name.last}
+            </h6>
+            <div>
+              <p>city: {user.location.city}</p>
+              <p>country: {user.location.country}</p>
+            </div>
+            <div>email: {user.email}</div>
+            <div>phone: {user.phone}</div>
+            <br />
+          </Fragment>
+        );
+      })}
+      <button>Add new User</button>
+    </>
+  );
 }
